fc3145e08babdbf3f1716ae76c724c57eae4427c
Ashikur Rahaman <stylozashik@gmail.com>
2021-08-01 10:52:09 +0600

    loss.py
    Writing error/loss function including mae,sse,mse,rmse

--- regresspy/loss.py
+++ regresspy/loss.py
@@ -4,52 +4,16 @@ from math import sqrt
 
 
 def mae(pred: ndarray, label: ndarray) -> ndarray:
-    """Returns the mean absolute error between the predicted values and the
-    actual values.
-
-    Args:
-        pred (ndarray): the array of predicted values.
-        label (ndarray): the array of ground truths.
-    Returns:
-        (ndarray): mean absolute errors.
-    """
     return np.sum(np.abs(pred - label)) / np.shape(pred)
 
 
 def sse(pred: ndarray, label: ndarray) -> ndarray:
-    """Returns the residual sum of squared errors between the predicted
-    values and the actual values.
-
-    Args:
-        pred (ndarray): the array of predicted values.
-        label (ndarray): the array of ground truths.
-    Returns:
-        (ndarray): residual sum of squared errors.
-    """
     return np.square(np.sum((pred - label) / np.shape(pred)))
 
 
 def mse(pred: ndarray, label: ndarray) -> ndarray:
-    """Returns the mean squared errors between the predicted
-    values and the actual values.
-
-    Args:
-        pred (ndarray): the array of predicted values.
-        label (ndarray): the array of ground truths.
-    Returns:
-        (ndarray): mean squared errors.
-    """
     return np.square(pred - label).mean()
 
 
 def rmse(pred: ndarray, label: ndarray) -> float:
-    """Returns the root mean squared error between the predicted values
-    and the actual values.
-
-    Args:
-        pred (ndarray): the array of predicted values.
-        label (ndarray): the array of ground truths.
-    Returns:
-        (ndarray): root mean squared errors.
-    """
     return sqrt(np.square(pred - label).mean())
