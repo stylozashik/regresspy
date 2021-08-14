aae3fa4070afdace010d8c7b6644205ae71159c9
Ashikur Rahaman <stylozashik@gmail.com>
2021-08-15 02:34:32 +0600

    Writing setup file for installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'regresspy',
+    version = '1.1',
+    packages = ['regresspy.Regression'],
+    package_dir = {'' : 'regresspy'},
+    url = 'http://example.com',
+    liscense = 'MIT',
+    author = "Ashikur Rahaman",
+    author_email = 'stylozashik@gmail.com',
+    description = 'Regression using Gradient Descent from scratch'
+)
