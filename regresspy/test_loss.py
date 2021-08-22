db13d866888601a399d0aabc3cb4ec85289f8606
Ashikur Rahaman <stylozashik@gmail.com>
2021-08-22 23:43:59 +0600

    Updating test_loss.py to import some missing function
    There was some function missing like mse and rmse, just imported all functions and fixed the problem as per as requirements.

--- regresspy/test_loss.py
+++ regresspy/test_loss.py
@@ -17,14 +17,14 @@ def test_sse():
 
 
 def test_mse():
-    dummy_predictions = np.array([2.0, 4.0, 7.0, 8.0])
+    dummy_predictions = np.array([3.0, 5.0, 8.0, 9.0])
     dummy_labels = np.array([1.0, 3.0, 6.0, 7.0])
-    dummy_mae = 1.0
+    dummy_mse = 4.0
     assert mse(dummy_predictions, dummy_labels) == dummy_mse
 
 
 def test_rmse():
-    dummy_predictions = np.array([2.0, 4.0, 7.0, 8.0])
+    dummy_predictions = np.array([3.0, 5.0, 8.0, 9.0])
     dummy_labels = np.array([1.0, 3.0, 6.0, 7.0])
-    dummy_mae = 1.0
+    dummy_rmse = 2.0
     assert rmse(dummy_predictions, dummy_labels) == dummy_rmse
